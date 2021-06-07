820bf5ab8056f131ec6313862318bc97da6c2bd1
Emilia Brzoskowska <emilia.brzoskowska@gmail.com>
2021-06-07 14:26:24 +0100

    new task

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const reverseString = (newString) => {
 
-    let myString = "Kurwa jego mac!";
+    let myString = "cholerka!";
     return newString = myString.split("").reverse().join("");
 };
 
 console.log(reverseString());
 
+let board = [
+    ['X', 'O', 'O'],
+    [' ', 'X', ' '],
+    [' ', ' ', 'X']
+];   
+         
+    
+let checkResult = (board, token) => { 
+    let row1 = (board[0][0] == board[0][1] && board[0][2] == token)
+    let row2 = (board[1][0] == board[0][1] && board[0][2] == token)
+    let row3 = (board[2][0] == board[0][1] && board[0][2] == token)
+    let col1 = (board[0][0] == board[1][0] && board[2][0] == token)
+    let col2 = (board[0][1] == board[1][1] && board[2][1] == token)
+    let col3 = (board[0][2] == board[1][2] && board[2][2] == token)
+    let dia1 = (board[0][0] == board[1][1] && board[2][2] == token)
+    let dia2 = (board[0][2] == board[1][1] && board[2][0] == token)
+
+    return row1 || row2 || row3 || col1 || col2 || col3 || dia1 || dia2    
+} 
+
+let result = checkResult(board, 'X') ? "Result: X is the WINNER!" : checkResult(board, 'O') ? "Result: O is the WINNER!" : "Result: DRAW!"
+console.log(result);
      
