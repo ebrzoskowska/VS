fe4aa102d35db654a29150fb2621cfae17042b85
Emilia Brzoskowska <emilia.brzoskowska@gmail.com>
2021-06-07 12:33:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const reverseString = (newString) => {
+
+    let myString = "Kurwa jego mac!";
+    return newString = myString.split("").reverse().join("");
+};
+
+console.log(reverseString());
+
+     
